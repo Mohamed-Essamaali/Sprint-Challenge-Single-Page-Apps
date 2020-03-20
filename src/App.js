db269bcf9b654d7e2077ddd3c3734e2cd15ce68b
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-03-20 19:58:02 -0400

    Displayed data, Search bar not yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import SearchForm from "./components/SearchForm.js";
+
 
 export default function App() {
   return (
     <div>
  <main data-testid='app'>
       <Header />
+      {/* <SearchForm /> */}
       <CharacterList/>
     </main>
     
